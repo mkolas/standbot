d2d27c5fd65e9e3a7f6e626cc207415b91e7308d
Max <mkolas@github.com>
2017-02-17 15:36:17 -0500

    implemented dumb stand message

--- index.js
+++ index.js
@@ -88,26 +88,24 @@ controller.setupWebserver(process.env.PORT, function (err, webserver) {
 controller.on('slash_command', function (slashCommand, message) {
 
     switch (message.command) {
-        case "/echo": //handle the `/echo` slash command. We might have others assigned to this app too!
+        case "/stand": //handle the `/echo` slash command. We might have others assigned to this app too!
             // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
             // Otherwise just echo back to them what they sent us.
 
-            // but first, let's make sure the token matches!
-            if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
-
             // if no text was supplied, treat it as a help command
             if (message.text === "" || message.text === "help") {
-                slashCommand.replyPrivate(message,
-                    "I echo back what you tell me. " +
-                    "Try typing `/echo hello` to see.");
+                slashCommand.replyPublic(message,
+                    "ＴＨＩＳ 　ＭＵＳＴ 　ＢＥ 　ＴＨＥ 　ＷＯＲＫ 　ＯＦ 　ＡＮ 　ＥＮＥＭＹ 「ＳＴＡＮＤ」！！");
                 return;
             }
 
+            var outputText = message.text.toUpperCase.split('').join(' ');
+
+            outputText = "`_ゴゴゴ_「 " + outputText + " ` 」_ゴゴゴ_ `";
+
             // If we made it here, just echo what the user typed back at them
             //TODO You do it!
-            slashCommand.replyPublic(message, "1", function() {
-                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-            });
+            slashCommand.replyPublic(message, outputText);
 
             break;
         default:
